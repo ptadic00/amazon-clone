21e31d735e283bb179d6b82eacf8c7ddbf2df7bf
Petra Tadić <Petra>
2021-03-25 22:06:58 +0100

    navigation bar & banner with product

---
+++ src/Header.js
@@ -0,0 +1,143 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import LocationOnIcon from '@material-ui/icons/LocationOn'
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <Container>
+            <HeaderLogo>
+                <Link to ="/">
+                    <img src={"https://mikekitko.com/wp-content/uploads/2019/10/amazon-logo-white-768x232.png"}/>
+                </Link>
+            </HeaderLogo>
+            
+            <HeaderOptionAdress>
+                <LocationOnIcon/>
+                <HeaderOption>
+                    <OptionLineOne>Hello,</OptionLineOne>
+                    <OptionLineTwo>Select your adress</OptionLineTwo>
+                </HeaderOption>
+            </HeaderOptionAdress>
+            
+            <HeaderSearch>
+                <HeaderSearchInput type="text"/>
+                <HeaderSearchIconContainer>
+                    <SearchIcon/>
+                </HeaderSearchIconContainer>
+            </HeaderSearch>
+
+            <HeaderNavItems>
+
+                <HeaderOption>
+                    <OptionLineOne>Hello, Petra</OptionLineOne>
+                    <OptionLineTwo>Accounts & Lists</OptionLineTwo>
+                </HeaderOption>
+
+                <HeaderOption>
+                    <OptionLineOne>Returns</OptionLineOne>
+                    <OptionLineTwo>& Orders</OptionLineTwo>
+                </HeaderOption>
+
+               
+                <HeaderOptionCard>
+                    <Link to="/cart">
+                        <ShoppingCartIcon/>
+                        <CartCounter>4</CartCounter>
+                    </Link>
+                </HeaderOptionCard>
+            </HeaderNavItems>
+        </Container>
+    )
+}
+
+export default Header
+
+
+
+const Container = styled.div`
+    height:60px;
+    background-color: #0F1111;
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    color:white;
+`
+const HeaderLogo=styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    img{
+        width:100px;
+        margin-left:11px;
+        margin-top:10px;
+    }
+`
+
+const HeaderOptionAdress = styled.span`
+    padding-left: 9px;
+    display:flex;
+    align-items:center;
+`
+
+const OptionLineOne = styled.div``
+
+const OptionLineTwo = styled.div`
+    font-weight: 700;
+`
+
+const HeaderSearchInput =styled.input`
+    flex-grow:1;
+    border:0;
+    :focus{
+        outline:none;
+    }
+`
+
+const HeaderSearch=styled.div`
+    display:flex;
+    flex-grow: 1;
+    height:40px;
+    border-radius: 4px;
+    overflow:hidden;
+    margin-left:4px;
+    background-color:white;
+    :focus-within{
+        box-shadow: 0 0 0 2px #F90;
+    }
+`
+
+const HeaderSearchIconContainer=styled.div`
+    background-color: #febd69;
+    width:45px;
+    color:black;
+    display:flex;
+    justify-content: center;
+    align-items:center;
+`
+
+const HeaderOption =styled.div`
+    padding: 10px 9px;
+`
+
+const HeaderNavItems =styled.div`
+    display:flex;
+
+`
+
+const HeaderOptionCard=styled.div`
+    display:flex;
+    a{
+        display:flex;
+        align-items:center;
+        padding-right:9px;
+        color:white;
+        text-decoration:none;
+    }
+`
+
+const CartCounter =styled.div`
+    padding-left: 4px;
+`
---
+++ src/Product.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Product() {
+    return (
+        <Container>
+            <Title>
+                Ipad Pro
+            </Title>
+
+            <Price>
+                $1449
+            </Price>
+
+            <Rating>
+                ⭐⭐⭐⭐⭐
+            </Rating>
+
+            <Image src='https://images-na.ssl-images-amazon.com/images/I/81SGb5l%2BlZL._AC_SX342_.jpg'/>
+            <ActionSection>
+                <AddToCartButton>
+                    Add to Cart
+                </AddToCartButton>
+            </ActionSection>
+            
+        </Container>
+    )
+}
+
+export default Product
+
+const Container=styled.div`
+    background-color:white;
+    z-index:100;
+    max-height:400px;
+    flex:1;
+    padding:20px;
+    margin:10px;
+    display:flex;
+    flex-direction:column;
+    border-radius:7px;
+`
+
+const Title=styled.span``
+const Price=styled.span`
+    font-weight:500;
+    margin-top:3px;
+`
+const Rating=styled.div``
+const Image=styled.img`
+    max-height:200px;
+    object-fit:contain;
+    margin:10px;
+`
+const AddToCartButton=styled.button`
+    width:100px;
+    height:30px;
+    background-color:#f0c14b;
+    border:2px solid #a88734;
+    border-radius:2px;
+`
+const ActionSection=styled.div`
+    margin-top:12px;
+    display:grid;
+    place-items:center;
+`
