0281af345ac2810020f3d8bded4af90bd1c65966
Petra <62611106+ptadic00@users.noreply.github.com>
2021-05-11 09:05:26 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC96SRGYcrVnl8hky53PAIWxUJoklgtdys",
-    authDomain: "clone-aeeec.firebaseapp.com",
-    projectId: "clone-aeeec",
-    storageBucket: "clone-aeeec.appspot.com",
-    messagingSenderId: "437930270113",
-    appId: "1:437930270113:web:fe13e2cbeaa681ff22ffe0"
+    /*secret*/
   });
 
   const db= firebase.firestore();
